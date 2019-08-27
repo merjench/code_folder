0fb0094a135d1629505f802183d0e3b0268bcae4
merjench <merjen.chary@gmail.com>
2019-08-27 14:53:38 -0500

    student and department solution sample 1

--- intel_aduit.js
+++ intel_aduit.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //sample pascal Triangle solution
+//////--------------Question 1 --------------------------/////
+
 
 function createPascalTriangle (numRows) {
   var pascalTriangle = [];
@@ -21,3 +23,27 @@ function createPascalTriangle (numRows) {
 }
 
 console.table(createPascalTriangle(6));
+
+//----------------Question 2 --------------------/////
+
+
+// 2) Students and Departments
+//
+//  A university uses 2 data tables, Students and Departments, to store data about its students and the
+// departments associated with each major. Write a query to print the respective department
+// name and number of students majoring in each department for all departments in
+// the Departments table (even ones with no current students). Sort your results by descending number
+// of students; if two or more departments have same number of students, then sort those departments
+// alphabetically by department name.
+
+// ----Solution 1----------------///
+
+SELECT
+    dept_name, COUNT(student_id) AS student_number
+FROM
+    department
+        LEFT OUTER JOIN
+    student ON department.dept_id = student.dept_id
+GROUP BY department.dept_name
+ORDER BY student_number DESC , department.dept_name
+;
